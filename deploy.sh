a263ec68c946b3e01694c46aa79e64489928f16c
SanRob1325 <seanrobertblitzntonya@gmail.com>
2025-03-05 21:47:04 +0000

    Added security

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 sudo apt update && sudo apt install nodejs npm
-sudo npm install -g pm2 # testing
+sudo npm install -g pm2
 pm2 stop example_app
 cd ExampleApplication/
 npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 pm2 start ./bin/www --name example_app
